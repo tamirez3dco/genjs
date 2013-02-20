a0d29f2839fe6a984908dd5b63eddc15c5127e23
Amit Aviv <amit@ez3d.co>
2013-02-20 02:58:55 +0200

    can't have anything from /client loaded in the worker.. We now have duplicate \"geometry\" files.. no good..

--- public/worker/code-worker.js
+++ public/worker/code-worker.js
@@ -1,35 +1,17 @@
-var isWorker = false;
-try {
-	importScripts('/worker/underscore-min.js');
-	importScripts('/worker/three-core1.js');
-	importScripts('/worker/toxiclibs.js');
-	importScripts('/worker/memoize.js');
-	importScripts('/worker/csg.js');
-	importScripts('/worker/math_functions.js');
-	importScripts('/worker/z.optimer_bold.typeface.js');
-	importScripts('/worker/z.optimer_regular.typeface.js');
-	importScripts('/client/lib/extend-geometry.js')
-	importScripts('/client/lib/geometry.js');
-	isWorker = true;
-} catch(err) {
-	console.log('Not a worker..');
-}
+importScripts('/worker/underscore-min.js');
+importScripts('/worker/three-core1.js');
+importScripts('/worker/toxiclibs.js');
+importScripts('/worker/memoize.js');
+importScripts('/worker/csg.js');
+importScripts('/worker/math_functions.js');
+importScripts('/worker/z.optimer_bold.typeface.js');
+importScripts('/worker/z.optimer_regular.typeface.js');
+importScripts('/worker/extend-geometry.js');
+importScripts('/worker/geometry.js');
 
-if(isWorker) {
-	importScripts('/worker/underscore-min.js');
-	importScripts('/worker/three-core1.js');
-	importScripts('/worker/toxiclibs.js');
-	importScripts('/worker/memoize.js');
-	importScripts('/worker/csg.js');
-	importScripts('/worker/math_functions.js');
-	importScripts('/worker/z.optimer_bold.typeface.js');
-	importScripts('/worker/z.optimer_regular.typeface.js');
-	importScripts('/client/lib/extend-geometry.js');
-	importScripts('/client/lib/geometry.js');
-	GEN.debug = new GEN.Debugger();
-	GEN.Geometry.initGlobal();
-	goog.memoize.USAGE_COUNTER = 0;
-}
+GEN.debug = new GEN.Debugger();
+GEN.Geometry.initGlobal();
+goog.memoize.USAGE_COUNTER = 0;
 
 var ports = [];
 onconnect = function(event) {
@@ -43,7 +25,7 @@ onconnect = function(event) {
 listenForMessage = function(event, port) {
 	var code = event.data;
 	GEN.debug.start();
-	
+
 	eval(code);
 	var renderableBlocks = encodeRenderables()
 	port.postMessage(JSON.stringify(renderableBlocks));
@@ -53,7 +35,7 @@ listenForMessage = function(event, port) {
 encodeRenderables = function() {
 	var blocksIds = _.keys(GEN.debug.tracedBlocks);
 	var renderableBlocks = {};
-	
+
 	_.each(blocksIds, function(id) {
 		renderableBlocks[id] = [];
 		var values = GEN.debug.tracedBlocks[id];
